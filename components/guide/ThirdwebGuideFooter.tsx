bb690ea74782216fbacc6e20be946b8c6099a8f8
wtdragon <gavin.wang.tao@gmail.com>
2022-12-14 00:36:37 +0800

    1st commit

---
+++ components/guide/ThirdwebGuideFooter.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function ThirdwebGuideFooter() {
+  return (
+    <>
+      <div
+        style={{
+          position: "fixed",
+          bottom: 14,
+          right: 18,
+        }}
+      ></div>
+    </>
+  );
+}
